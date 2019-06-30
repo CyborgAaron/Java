147d931a6720e7734d76c8b1a7b9246cb759533e
Aaron <aml854@live.co.uk>
2019-06-30 22:32:09 +0100

    Update CalculateFibonacci.java

--- src/Module9/CalculateFibonacci.java
+++ src/Module9/CalculateFibonacci.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class CalculateFibonacci {
     static long called1 = 0;
-    //static int count = 0;
 
     public static void main(String args[]) {
         // Create a Scanner
@@ -18,16 +17,6 @@ public class CalculateFibonacci {
 
         System.out.println(" Number of times the method has been called " + index + numOfTimes(index) +
                 " This has been counted " + called1);
-
-        /*
-        *  // Find and display the Fibonacci number
-        System.out.println(
-                "Fibonacci number at index " + index + " is " + fib(index));
-
-         // Find how many times the method is called
-
-        System.out.println("The fib method was called " + count + " times.");
-        * */
     }
 
 
@@ -51,20 +40,6 @@ public class CalculateFibonacci {
             called1++;
         }
             return called; //* numOfTimes(called++ - 1);
-//        for ( int i = 0; i < numOfTimes(numOfFib); i++) {
-//            numOfFib++;
-//        }
-//        return numOfTimes(called + 1) + numOfTimes(called * 2);
     }
-
-   /* public static long fib(long index) {
-        count++;
-        if (index == 0) // Base case
-            return 0;
-        else if (index == 1) // Base case
-            return 1;
-        else  // Reduction and recursive calls
-            return fib(index - 1) + fib(index - 2);
-    }*/
 
 }
